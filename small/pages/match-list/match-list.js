dfd665b81820140afa16d164ce1ca05b342e90ea
Deeper-Lee <984130957@qq.com>
2018-06-19 16:35:25 +0800

    赛事列表

--- small/pages/match-list/match-list.js
+++ small/pages/match-list/match-list.js
@@ -7,12 +7,17 @@ Page({
     load: true,
     match: {
       index: 0,
-      arr: ['所有赛事', '进行中', '报名中', '已结束', '待开始']
+      arr: ['所有赛事', '进行中', '报名中', '已结束', '待开始'],
     },
     matchType: {
       index: 0,
-      arr: ['所有类型' ,'足球', '篮球', '冰球', '乒乓球1']
-    }
+      arr: ['所有类型' ,'足球', '篮球', '冰球', '乒乓球'],
+    },
+    LayerLock: false,
+    matchLock: false,
+    matchIndex:0,
+    matchTypeLock: false,
+    matchTypeIndex:0
   },
 
   /**
@@ -42,7 +47,6 @@ Page({
       }
     });
     this.mobileInfo();
-    this.floor();
   },
 
   /**
@@ -132,8 +136,48 @@ Page({
   /**
    * 遮罩层
    */
-  floor: function (){
-    var query = wx.createSelectorQuery();
-    console.log(query)
+  selectMatch: function (event){
+    var matches = event.currentTarget.dataset.type;
+    if (matches == 'matchLock'){
+      this.setData({
+        matchTypeLock: false,
+        LayerLock: true,
+        matchLock: true,
+      })
+    } else if (matches == 'matchTypeLock'){
+      this.setData({
+        matchLock: false,
+        LayerLock: true,
+        matchTypeLock: true,
+      })
+    }
+    
+  },
+  LayerHide: function (){
+    this.setData({
+      LayerLock: false,
+      matchLock: false,
+      matchTypeLock: false
+    })
+  },
+  selectListIndex: function (event){
+    var obj = event.currentTarget.dataset;
+    console.log(obj);
+    if (obj.type == 'matchLock'){
+      this.setData({
+        matchIndex: obj.index,
+        LayerLock: false,
+        matchLock: false,
+        matchTypeLock: false        
+      })
+    } else if (obj.type == 'matchTypeLock'){
+      this.setData({
+        matchTypeIndex: obj.index,
+        LayerLock: false,
+        matchLock: false,
+        matchTypeLock: false        
+      })
+    }
+
   }
 })
